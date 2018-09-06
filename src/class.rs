a4fce29aa411d143f24dee44200c1faebeca6205
朱博文 <di3663@dingtalk.com>
2018-09-06 15:33:52 +0800

    .

---
+++ src/class.rs
@@ -0,0 +1,245 @@
+use std;
+
+use jni_sys::*;
+
+#[derive(Debug)]
+pub struct Class(jclass);
+
+impl Class {
+  pub(crate) fn from_handle(environment: &::Environment, handle: jclass) -> Class {
+    return Class(environment.retain(handle));
+  }
+
+  pub fn find(name: &str) -> Result<::Class, ::Object> {
+    let environment = ::get_env();
+
+    return environment.find_class(name);
+  }
+
+  pub fn get_superclass(&self) -> Option<Class> {
+    let environment = ::get_env();
+    let env = environment.as_handle();
+
+    let handle = unsafe { (**env).GetSuperclass.unwrap()(env, self.0) };
+
+    if handle.is_null() {
+      return None;
+    } else {
+      return Some(Class::from_handle(&environment, handle));
+    }
+  }
+
+  pub fn get_method(&self, name: &str, signature: &str) -> Result<::Method, ::Object> {
+    let environment = ::get_env();
+    let env = environment.as_handle();
+
+    let name = std::ffi::CString::new(name).unwrap();
+    let signature = std::ffi::CString::new(signature).unwrap();
+
+    let handle =
+      unsafe { (**env).GetMethodID.unwrap()(env, self.0, name.as_ptr(), signature.as_ptr()) };
+
+    match environment.check_jvm_exception() {
+      Some(e) => return Err(e),
+      None => return Ok(::Method::from_handle(handle)),
+    }
+  }
+
+  pub fn get_static_method(&self, name: &str, signature: &str) -> Result<::Method, ::Object> {
+    let environment = ::get_env();
+    let env = environment.as_handle();
+
+    let name = std::ffi::CString::new(name).unwrap();
+    let signature = std::ffi::CString::new(signature).unwrap();
+
+    let handle =
+      unsafe { (**env).GetStaticMethodID.unwrap()(env, self.0, name.as_ptr(), signature.as_ptr()) };
+
+    match environment.check_jvm_exception() {
+      Some(e) => return Err(e),
+      None => return Ok(::Method::from_handle(handle)),
+    }
+  }
+
+  pub unsafe fn call_void_method(
+    &self,
+    method: &::Method,
+    arguments: &[&::Value],
+  ) -> Result<(), ::Object> {
+    let environment = ::get_env();
+    let env = environment.as_handle();
+
+    let args: Vec<jvalue> = arguments.iter().map(|x| x.as_handle()).collect();
+
+    (**env).CallStaticVoidMethodA.unwrap()(env, self.0, method.as_handle(), args.as_ptr());
+
+    match environment.check_jvm_exception() {
+      Some(e) => return Err(e),
+      None => return Ok(()),
+    }
+  }
+
+  pub unsafe fn call_boolean_method(
+    &self,
+    method: &::Method,
+    arguments: &[&::Value],
+  ) -> Result<bool, ::Object> {
+    let environment = ::get_env();
+    let env = environment.as_handle();
+
+    let args: Vec<jvalue> = arguments.iter().map(|x| x.as_handle()).collect();
+
+    let result =
+      (**env).CallStaticBooleanMethodA.unwrap()(env, self.0, method.as_handle(), args.as_ptr());
+
+    match environment.check_jvm_exception() {
+      Some(e) => return Err(e),
+      None => return Ok(result != 0),
+    }
+  }
+
+  pub unsafe fn call_byte_method(
+    &self,
+    method: &::Method,
+    arguments: &[&::Value],
+  ) -> Result<i8, ::Object> {
+    let environment = ::get_env();
+    let env = environment.as_handle();
+
+    let args: Vec<jvalue> = arguments.iter().map(|x| x.as_handle()).collect();
+
+    let result =
+      (**env).CallStaticByteMethodA.unwrap()(env, self.0, method.as_handle(), args.as_ptr());
+
+    match environment.check_jvm_exception() {
+      Some(e) => return Err(e),
+      None => return Ok(result),
+    }
+  }
+
+  pub unsafe fn call_short_method(
+    &self,
+    method: &::Method,
+    arguments: &[&::Value],
+  ) -> Result<i16, ::Object> {
+    let environment = ::get_env();
+    let env = environment.as_handle();
+
+    let args: Vec<jvalue> = arguments.iter().map(|x| x.as_handle()).collect();
+
+    let result =
+      (**env).CallStaticShortMethodA.unwrap()(env, self.0, method.as_handle(), args.as_ptr());
+
+    match environment.check_jvm_exception() {
+      Some(e) => return Err(e),
+      None => return Ok(result),
+    }
+  }
+
+  pub unsafe fn call_int_method(
+    &self,
+    method: &::Method,
+    arguments: &[&::Value],
+  ) -> Result<i32, ::Object> {
+    let environment = ::get_env();
+    let env = environment.as_handle();
+
+    let args: Vec<jvalue> = arguments.iter().map(|x| x.as_handle()).collect();
+
+    let result =
+      (**env).CallStaticIntMethodA.unwrap()(env, self.0, method.as_handle(), args.as_ptr());
+
+    match environment.check_jvm_exception() {
+      Some(e) => return Err(e),
+      None => return Ok(result),
+    }
+  }
+
+  pub unsafe fn call_long_method(
+    &self,
+    method: &::Method,
+    arguments: &[&::Value],
+  ) -> Result<i64, ::Object> {
+    let environment = ::get_env();
+    let env = environment.as_handle();
+
+    let args: Vec<jvalue> = arguments.iter().map(|x| x.as_handle()).collect();
+
+    let result =
+      (**env).CallStaticLongMethodA.unwrap()(env, self.0, method.as_handle(), args.as_ptr());
+
+    match environment.check_jvm_exception() {
+      Some(e) => return Err(e),
+      None => return Ok(result),
+    }
+  }
+
+  pub unsafe fn call_float_method(
+    &self,
+    method: &::Method,
+    arguments: &[&::Value],
+  ) -> Result<f32, ::Object> {
+    let environment = ::get_env();
+    let env = environment.as_handle();
+
+    let args: Vec<jvalue> = arguments.iter().map(|x| x.as_handle()).collect();
+
+    let result =
+      (**env).CallStaticFloatMethodA.unwrap()(env, self.0, method.as_handle(), args.as_ptr());
+
+    match environment.check_jvm_exception() {
+      Some(e) => return Err(e),
+      None => return Ok(result),
+    }
+  }
+
+  pub unsafe fn call_double_method(
+    &self,
+    method: &::Method,
+    arguments: &[&::Value],
+  ) -> Result<f64, ::Object> {
+    let environment = ::get_env();
+    let env = environment.as_handle();
+
+    let args: Vec<jvalue> = arguments.iter().map(|x| x.as_handle()).collect();
+
+    let result =
+      (**env).CallStaticDoubleMethodA.unwrap()(env, self.0, method.as_handle(), args.as_ptr());
+
+    match environment.check_jvm_exception() {
+      Some(e) => return Err(e),
+      None => return Ok(result),
+    }
+  }
+
+  pub unsafe fn call_object_method(
+    &self,
+    method: &::Method,
+    arguments: &[&::Value],
+  ) -> Result<Option<::Object>, ::Object> {
+    let environment = ::get_env();
+    let env = environment.as_handle();
+
+    let args: Vec<jvalue> = arguments.iter().map(|x| x.as_handle()).collect();
+
+    let handle =
+      (**env).CallStaticObjectMethodA.unwrap()(env, self.0, method.as_handle(), args.as_ptr());
+
+    match environment.check_jvm_exception() {
+      Some(e) => return Err(e),
+      None => if handle.is_null() {
+        return Ok(None);
+      } else {
+        return Ok(Some(::Object::from_handle(&environment, handle)));
+      },
+    }
+  }
+}
+
+impl Drop for Class {
+  fn drop(&mut self) {
+    let env = ::get_env().as_handle();
+
+    unsafe { (**env).DeleteGlobalRef.unwrap()(env, self.0) };
+  }
+}
---
+++ src/value.rs
@@ -0,0 +1,70 @@
+use std;
+
+use jni_sys::*;
+
+#[derive(Clone)]
+pub struct Value<'a> {
+  handle: jvalue,
+  _data: std::marker::PhantomData<&'a jvalue>,
+}
+
+impl<'a> Value<'a> {
+  pub(crate) fn from_handle(handle: jvalue) -> Value<'a> {
+    return Value {
+      handle: handle,
+      _data: std::marker::PhantomData,
+    };
+  }
+
+  pub(crate) fn as_handle(&self) -> jvalue {
+    return self.handle;
+  }
+
+  pub fn from_nil() -> Value<'a> {
+    return Value::from_handle(jvalue {
+      l: std::ptr::null_mut(),
+    });
+  }
+
+  pub fn from_boolean(value: bool) -> Value<'a> {
+    return Value::from_handle(jvalue {
+      z: if value { 1 } else { 0 },
+    });
+  }
+
+  pub fn from_i8(value: i8) -> Value<'a> {
+    return Value::from_handle(jvalue { b: value });
+  }
+
+  pub fn from_i16(value: i16) -> Value<'a> {
+    return Value::from_handle(jvalue { s: value });
+  }
+
+  pub fn from_i32(value: i32) -> Value<'a> {
+    return Value::from_handle(jvalue { i: value });
+  }
+
+  pub fn from_i64(value: i64) -> Value<'a> {
+    return Value::from_handle(jvalue { j: value });
+  }
+
+  pub fn from_f32(value: f32) -> Value<'a> {
+    return Value::from_handle(jvalue { f: value });
+  }
+
+  pub fn from_f64(value: f64) -> Value<'a> {
+    return Value::from_handle(jvalue { d: value });
+  }
+
+  pub fn from_object(value: &::Object) -> Value<'a> {
+    return Value::from_handle(jvalue {
+      l: value.as_handle(),
+    });
+  }
+
+  pub fn from_string(value: &::String) -> Value<'a> {
+    return Value::from_handle(jvalue {
+      l: value.as_handle(),
+    });
+  }
+}
